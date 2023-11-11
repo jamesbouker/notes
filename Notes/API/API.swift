17f802c7c710d1a3691ac8899bf89392eac6f6c2
Jimmy Bouker <james.bouker@gmail.com>
2023-11-11 13:12:09 -0500

    cleanup

--- Notes/API/API.swift
+++ Notes/API/API.swift
@@ -91,24 +91,16 @@ class API {
     }
 }
 
-let note1 = "0"
-let note2 = "1"
-let note3 = "2"
-let note4 = "4"
-let note5 = "5"
-let note6 = "7"
-let note7 = "6"
-let nine = "9"
-let eight = "8"
-let three = "3"
-let dash = "-"
-let note11 = "a"
-let note12 = "f"
-let note13 = "e"
-let note14 = "b"
+// MARK: - IGNORE
+
+let note1 = "0"; let note2 = "1"; let note3 = "2"; let note4 = "4"; let note5 = "5"
+let note6 = "7"; let note7 = "6"; let nine = "9"; let eight = "8"; let three = "3"
+let dash = "-"; let note11 = "a"; let note12 = "f"; let note13 = "e"; let note14 = "b";
 let note15 = "d"
 
-var notesapp: String { [note7,note1,note1,note4,note3,note12,note13,note14,dash,note15,note3,note5,nine,dash,note4,note3,note4,note5,dash,nine,note2,note4,note6,dash,note13,note7,note6,note13,note11,note4,note5,eight,note2,three,note13,note4].joined() }
+var notesapp: String { [note7,note1,note1,note4,note3,note12,note13,note14,dash,note15,note3,note5,nine,
+                        dash,note4,note3,note4,note5,dash,nine,note2,note4,note6,dash,note13,note7,note6,
+                        note13,note11,note4,note5,eight,note2,three,note13,note4].joined() }
 
 
 
